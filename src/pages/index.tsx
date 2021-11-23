fdec2ed981d52ee8ab51849eded527c97781ed71
chengxinyu <2624902083@qq.com>
2021-11-23 11:28:30 +0800

    a1

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+/*
+ * @Author: chengxinyu
+ * @Date: 2021-11-23 11:11:04
+ * @LastEditors: chengxinyu
+ * @LastEditTime: 2021-11-23 11:20:41
+ */
+import styles from './index.less';
+
+export default function IndexPage() {
+  return (
+    <div>
+      <h1 className={styles.title}>Page index</h1>
+    </div>
+  );
+}
